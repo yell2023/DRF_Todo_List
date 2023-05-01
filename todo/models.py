1ae1d77339b524fe7948afa02aa33c82032261b9
Yang ye lin <127134844+yell2023@users.noreply.github.com>
2023-05-01 16:56:44 +0900

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from users.models import User
 
 class Todo(models.Model):
-    uer = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     is_complete = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
